ac630d70392f9fd1564500e55c0f2f1d283a7d3c
Taimur <tsohrab8@gmail.com>
2017-06-27 13:19:06 -0400

    first commit

---
+++ src/Division.java
@@ -0,0 +1,28 @@
+//Taimur Sohrab
+import java.util.Scanner;
+ 
+class Division {
+  public static void main(String[] args) {
+ 
+	  int a, b, result;
+	 
+	  Scanner input = new Scanner(System.in);
+	  System.out.println("Input two integers");
+	 
+	  a = input.nextInt();
+	  b = input.nextInt();
+	 
+	  try{
+		  if (b == 0) {
+		    throw new IllegalArgumentException("Divisor is 0");
+		  }
+		  
+		  result = a / b;
+		  System.out.println("Result = " + result);
+	  }
+	  catch(IllegalArgumentException e){
+		  System.out.println("Error: You cannot divide by 0!");
+	  }
+  
+  }
+}
